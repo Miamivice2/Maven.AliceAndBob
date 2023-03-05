71f8d2408ed57bc56665580f40ed4a5fddc8a052
Andre <andre@Zipcoders-MacBook-Pro-55.local>
2023-03-05 18:14:20 -0500

    alice and bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +7,25 @@ public class Main {
 
     public static void main(String[] args ){
 
+        Scanner scan = new Scanner(System.in);
+
+        while (true){
+
+
+        System.out.print("whats your name:");
+        String name = scan.next();
+
+        if(name.equals("Alice") ||name.equals("Bob")){
+            System.out.println("How are you");
+
+            }else{
+            System.out.println("You are lying");
+            }
+
+            }
     }
 }
+
+
+
+
